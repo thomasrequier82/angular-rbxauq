45663e512469b818b8427e6db4464e871c83fa6d
thomasrequier82 <thomas.requier@orange.fr>
2022-01-24 10:25:37 +0100

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
