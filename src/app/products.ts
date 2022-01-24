186ed98cfd46c4a6ec95b90261495b1169c1a2e6
thomasrequier82 <thomas.requier@orange.fr>
2022-01-24 10:38:57 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,19 +8,19 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'ARCHOS X67 5G',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'Archos 50 Saphir Anti-Choc',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'ARCHOS SAPHIR 24F',
     price: 299,
     description: ''
   }
