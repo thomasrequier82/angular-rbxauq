b807160ae52cbdf525dda07035c5b0782618be77
thomasrequier82 <thomas.requier@orange.fr>
2022-01-24 11:17:34 +0100

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,7 +13,8 @@ export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   constructor(
     private route: ActivatedRoute,
-    private cartService: CartService) { }
+    private cartService: CartService
+    ) { }
 
   ngOnInit(): void {
     // First get the product id from the current route.
@@ -23,7 +24,7 @@ export class ProductDetailsComponent implements OnInit {
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
   }
-  
+
   addToCart(product: Product) {
     this.cartService.addToCart(product);
     window.alert('Your product has been added to the cart!');
